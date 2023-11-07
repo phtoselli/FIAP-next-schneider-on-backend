01226fae686f24c1460365e6b1fc0d1155dc1549
phtoselli <phtoselli@gmail.com>
2023-11-07 11:24:42 -0300

    Add: create basic node server http  structure

---
+++ src/index.js
@@ -0,0 +1,34 @@
+import http from "node:http";
+
+const PORT = 3001;
+
+const routes = {
+    "/": {
+    "GET": (_req, res) => {
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.write(JSON.stringify({ "message": "OK" }));
+      res.end();
+    }
+  },
+  default: (_req, res) => {
+    res.writeHead(404, { "Content-Type": "application/json" });
+    res.write(JSON.stringify({ "message": "This route doesn't exists." }));
+    res.end();
+  }
+};
+
+const handler = (req, res) => {
+  const { url, method } = req;
+  console.log("url:", url);
+  console.log("method:", method);
+
+  if (!routes[url] || !routes[url][method]) return routes.default(req, res);
+
+  return routes[url][method](req, res);
+};
+
+const server = http
+  .createServer(handler)
+  .listen(PORT, () => console.log(`🟢 Node http server is running on ${PORT}`));
+
+export default server;
